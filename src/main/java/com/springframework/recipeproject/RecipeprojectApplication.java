ff5195ae8574e61087e8c4d481abb43ce237de30
Ashraf Makki <makkiashraf08@gmail.com>
2022-07-11 18:43:41 -0400

    Initializing data with spring. Closes #6

--- src/main/java/com/springframework/recipeproject/RecipeprojectApplication.java
+++ src/main/java/com/springframework/recipeproject/RecipeprojectApplication.java
@@ -2,6 +2,7 @@ package com.springframework.recipeproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class RecipeprojectApplication {
