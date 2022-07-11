584c0accaec0276dba1bef9b9339c9bbf1a13687
Ashraf Makki <makkiashraf08@gmail.com>
2022-07-11 14:54:02 -0400

    Added ManytoMany relationships. Closes #3

--- src/main/java/com/springframework/recipeproject/domain/Recipe.java
+++ src/main/java/com/springframework/recipeproject/domain/Recipe.java
@@ -9,7 +9,10 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
@@ -27,7 +30,7 @@ public class Recipe {
 	private String source;
 	private String url;
 	private String directions;
-//	private Difficulty diffuclty;
+
 	
 	@OneToMany(cascade = CascadeType.ALL)
 	private Set<Ingredients> ingredients;
@@ -40,6 +43,12 @@ public class Recipe {
 	
 	@OneToOne(cascade = CascadeType.ALL)
 	private Notes notes;
+	
+	@ManyToMany
+	@JoinTable(name = "recipe_category",
+		joinColumns = @JoinColumn(name = "recipe_id"),
+	    inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories;
 	
 	public Long getId() {
 		return id;
@@ -107,7 +116,18 @@ public class Recipe {
 	public void setIngredients(Set<Ingredients> ingredients) {
 		this.ingredients = ingredients;
 	}
-	
-	
+	public Difficulty getDifficulty() {
+		return difficulty;
+	}
+	public void setDifficulty(Difficulty difficulty) {
+		this.difficulty = difficulty;
+	}
+	public Set<Category> getCategories() {
+		return categories;
+	}
+	public void setCategories(Set<Category> categories) {
+		this.categories = categories;
+	}
+
 
 }
