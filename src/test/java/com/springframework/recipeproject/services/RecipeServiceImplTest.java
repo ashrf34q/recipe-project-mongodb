f12a6e7ad898c54b5f9dee87be39c8a8c40011f5
Ashraf Makki <makkiashraf08@gmail.com>
2022-07-19 13:21:14 -0400

    Adding mocks in our tests. closes #14

---
+++ src/test/java/com/springframework/recipeproject/services/RecipeServiceImplTest.java
@@ -0,0 +1,49 @@
+package com.springframework.recipeproject.services;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.springframework.recipeproject.domain.Recipe;
+import com.springframework.recipeproject.repositories.RecipeRepository;
+
+public class RecipeServiceImplTest {
+	
+	 RecipeServiceImpl recipeService;
+	 
+	 @Mock
+	 RecipeRepository recipeRepository;
+	 
+	 @Before
+	 public void setUp() throws Exception{
+		MockitoAnnotations.openMocks(this);
+		
+		recipeService = new RecipeServiceImpl(recipeRepository);		
+		
+	 }
+
+	@Test
+	public void testGetRecipes() throws Exception {
+		
+		Recipe recipe = new Recipe();
+		HashSet<Recipe> recipesData = new HashSet<>();
+		recipesData.add(recipe);
+		
+		when(recipeRepository.findAll()).thenReturn(recipesData);
+		
+		Set<Recipe> recipes = recipeService.getRecipes();
+		
+		assertEquals(recipes.size(), 1);
+		verify(recipeRepository, times(1)).findAll();
+	}
+
+}
