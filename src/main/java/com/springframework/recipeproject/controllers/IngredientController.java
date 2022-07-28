d07ff4de6c0f89edb90266fa92f35d48ef7e506e
Ashraf Makki <makkiashraf08@gmail.com>
2022-07-28 15:44:35 -0400

    Implemented view. Closes #36

--- src/main/java/com/springframework/recipeproject/controllers/IngredientController.java
+++ src/main/java/com/springframework/recipeproject/controllers/IngredientController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.springframework.recipeproject.commands.IngredientsCommand;
@@ -60,6 +61,8 @@ public class IngredientController {
 		return "recipe/ingredient/ingredientForm";
 	}
 	
+	
+	@PostMapping
 	@RequestMapping("/recipe/{recipeId}/ingredient")
 	public String saveOrUpdate(@ModelAttribute IngredientsCommand command) {
 		IngredientsCommand savedCmd = ingredientService.saveIngredientCommand(command);
